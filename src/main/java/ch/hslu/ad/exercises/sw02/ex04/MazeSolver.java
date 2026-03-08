5dfce6bd928ba7d8aadaac1a5b53d13da3b4097b
Cédric Müller <cedimueller@outlook.de>
2026-03-08 17:43:30 +0100

    D1_EX implement maze solver

--- src/main/java/ch/hslu/ad/exercises/sw02/ex04/MazeSolver.java
+++ src/main/java/ch/hslu/ad/exercises/sw02/ex04/MazeSolver.java
@@ -1,36 +1,67 @@
 package ch.hslu.ad.exercises.sw02.ex04;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MazeSolver {
 
-    private char[][] maze;
+    private final char[][] maze;
+
+    private int count;
 
     public MazeSolver(char[][] maze) {
         this.maze = maze;
+        this.count = 0;
+
+        findPath(0, 0);
+
+        System.out.println(count);
     }
 
-    public static void main(String[] args) {
-        char[][] mazeExample = {
-                {' ', ' ', ' ', '#', '#', '#', '#', '#', '#', '#'},
-                {' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', '#', '#'},
-                {' ', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#'},
-                {' ', '#', ' ', '#', ' ', ' ', ' ', '#', ' ', '#'},
-                {'#', '#', ' ', '#', '#', '#', ' ', '#', ' ', '#'},
-                {'#', '#', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', ' ', ' ', '#', '#', '#', '#', '#', '#', ' '},
-                {' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-                {' ', '#', '#', '#', '#', '#', '#', ' ', '#', '#'},
-                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X'}};
-
-        MazeSolver solver = new MazeSolver(mazeExample);
-
-        // TODO
+    public static void main(String[] args) throws IOException {
+        Path file = Path.of("src/main/java/ch/hslu/ad/exercises/sw02/ex04/largeMaze.txt");
+        List<String> lines = Files.readAllLines(file);
+        char[][] maze = new char[lines.size()][];
+        for (int i = 0; i < lines.size(); i++) {
+            maze[i] = lines.get(i).toCharArray();
+        }
+        MazeSolver solver = new MazeSolver(maze);
     }
 
-    public void findPath(int a, int b) { // TODO: Bessere Parameternamen?
-        // TODO
+    /**
+     * West > Ost > Nord > Süd
+     */
+    public void findPath(int row, int column) {
+//        if (count >= 1) { // adjusted base case - terminates after elem has been foun
+//            return;
+//        }
+        if (row < 0 || row >= maze.length || column < 0 || column >= maze[row].length) {
+            return;
+        }
+        if (maze[row][column] == 'X') {
+            printMaze();
+            return;
+        }
+        if (maze[row][column] == ' ') { // boolean short circuit would be more performant - since the call stack is resolved instantly
+            maze[row][column] = '.';
+            findPath(row, column - 1); // West
+            findPath(row, column + 1); // Ost
+            findPath(row - 1, column); // Nord
+            findPath(row + 1, column); // Süd
+            maze[row][column] = ' ';
+        }
     }
 
     private void printMaze() {
-        // TODO
+        count++;
+        for (char[] chars : maze) {
+            for (char aChar : chars) {
+                System.out.print(aChar + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
